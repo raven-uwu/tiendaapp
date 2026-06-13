6309bb270c4c3299859ae2e8d18855e49e26d869
raven-uwu <maria.jose.araya3564@gmail.com>
2026-06-13 14:00:28 -0600

    chore: setup inicial con Spring Initializr

---
+++ src/main/java/com/ufide/tiendaapp/TiendaappApplication.java
@@ -0,0 +1,13 @@
+package com.ufide.tiendaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaappApplication.class, args);
+	}
+
+}
